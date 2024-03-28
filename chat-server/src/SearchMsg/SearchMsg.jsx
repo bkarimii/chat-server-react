c03c7e040c17bf5f527344f898a548537a9fa79a
karimi <behrouzkarimi1993@gmail.com>
2024-03-28 20:38:57 +0000

    search for messages component done

--- chat-server/src/SearchMsg/SearchMsg.jsx
+++ chat-server/src/SearchMsg/SearchMsg.jsx
@@ -1,20 +1,57 @@
-import { react, useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 
 export default function SearchMsg() {
   const [allMessages, setAllMessages] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
 
   async function getAllMessages(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    setAllMessages(data);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const finalSearchTerm = searchTerm;
+    const data = await getAllMessages(
+      `http://localhost:9090/messages/search?text=${finalSearchTerm}`
+    );
+    setAllMessages(data);
+    allMessages.map((object, index) => {
+      return (
+        <ul>
+          <li key={index}>{object.text}</li>
+        </ul>
+      );
+    });
+  };
+  const handleInput = (e) => {
+    setsearchTerm(e.target.value);
+  };
   return (
     <>
-      <form id="search-form">
-        <input type="text" placeholder="Search for any word" />
+      <form id="search-form" onSubmit={(e) => handleSearch(e)}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleInput}
+          placeholder="Search for any word"
+        />
         <button type="submit">Search</button>
       </form>
+      <div>
+        <ul>
+          {allMessages.length > 0 &&
+            allMessages.map((obj, index) => {
+              return <li key={index}>{obj.text}</li>;
+            })}
+        </ul>
+      </div>
     </>
   );
 }
