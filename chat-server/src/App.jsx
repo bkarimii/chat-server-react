81b611f9aee034652b70371a47e671da5cacbca3
karimi <behrouzkarimi1993@gmail.com>
2024-03-30 20:59:58 +0000

    adding delete method fetch to delete from server

--- chat-server/src/App.jsx
+++ chat-server/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import MsgForm from "./MsgForm/MsgForm";
 import SearchMsg from "./SearchMsg/SearchMsg";
-import SearchById from "./SearchById/SearchById";
+import SearchById from "./SearchDeleteById/SearchDeleteById";
 import "./App.css";
 
 export default function App() {
--- chat-server/src/SearchDeleteById/SearchDeleteById.jsx
+++ chat-server/src/SearchDeleteById/SearchDeleteById.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "./SearchById.css";
+import "./SearchDeleteById.css";
 
 export default function SearchById() {
   //this var set id of the msg user searches for
   const [msgId, setMsgId] = useState("");
 
-  //this is the msg comes back from server with the searched id
+  //this is the msg comes back from the server with the corresponding id
   const [msgOfId, setMsgOfId] = useState({});
+  const [status, setStatus] = useState("");
 
+  //this fetch function delete a specific msg from server
+  const handleDeleteClick = async (e) => {
+    try {
+      const response = await fetch(`http://localhost:9090/messages/${msgId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        setStatus("Delete successful");
+        console.log(response.json());
+      } else {
+        setStatus("couldn't delete it");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //this function fetch the message with a specific id
   const findMsg = async (url) => {
     try {
       const response = await fetch(url);
@@ -52,7 +71,8 @@ export default function SearchById() {
         value={msgId}
         onChange={handleInput}
       />
-      <button onClick={(e) => handleClick(e)}>Search By Message ID</button>
+      <button onClick={(e) => handleClick(e)}>Search it</button>
+      <button onClick={(e) => handleDeleteClick(e)}>Delete it</button>
       {Object.keys(msgOfId).length > 0 ? displayResults(msgOfId) : null}
     </>
   );
